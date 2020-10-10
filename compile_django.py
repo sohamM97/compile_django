4ea28b4cf7c05a26bb781a31d9d02c554882217f
sohamM97 <soham.marik@gmail.com>
2020-10-10 22:26:02 +0530

    Fixed cpython version issue

--- compile_django.py
+++ compile_django.py
@@ -1,26 +1,40 @@
 import os, sys, shutil, compileall
 
-try:
-	old_proj = sys.argv[1]
-except IndexError:
-	raise Exception("Missing: project to be compiled")
-try:
-	new_proj = sys.argv[2]
-except IndexError:
-	new_proj = old_proj+'_compiled'
+def compile_project(old_proj, new_proj=None):
 
-if os.path.exists(new_proj):
-	shutil.rmtree(new_proj)
-shutil.copytree(old_proj, new_proj)
+	if new_proj is None:
+		new_proj = old_proj+'_compiled'
 
-compileall.compile_dir(new_proj)
+	if os.path.exists(new_proj):
+		shutil.rmtree(new_proj)
+	shutil.copytree(old_proj, new_proj)
 
-for folder, subfolders, files in os.walk(new_proj):
-	for file in files:
-		if file.endswith('.py'):
-			file_name = file[:-3]
-			os.remove(os.path.join(folder, file))
-			os.rename(
-				os.path.join(folder, '__pycache__',file_name+'.cpython-36.pyc'),
-				os.path.join(folder,file_name+'.pyc')
-			)
+	compileall.compile_dir(new_proj)
+
+	for folder, subfolders, files in os.walk(new_proj):
+		for file in files:
+			if file.endswith('.py'):
+				file_name = file[:-3]
+				os.remove(os.path.join(folder, file))
+				os.rename(
+					os.path.join(
+						folder, 
+						'__pycache__',
+						file_name+'.'+sys.implementation.cache_tag+'.pyc'
+					),
+					os.path.join(folder,file_name+'.pyc')
+				)
+
+
+if __name__ == '__main__':
+	
+	try:
+		old_proj = sys.argv[1]
+	except IndexError:
+		raise Exception("Missing: project to be compiled")
+	try:
+		new_proj = sys.argv[2]
+	except IndexError:
+		new_proj = old_proj+'_compiled'
+
+	compile_project(old_proj, new_proj)
